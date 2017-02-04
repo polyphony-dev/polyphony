3a0f55f687ed3190237706a5cd52c3c8aa637eb3
Hiroaki Kataoka <kataoka@sinby.com>
2017-02-04 17:51:16 +0900

    Add ifexp tests

---
+++ tests/if/ifexp01.py
@@ -0,0 +1,12 @@
+from polyphony import testbench
+
+def ifexp01(x, y):
+    return True if x == y else False
+
+@testbench
+def test():
+    assert False == ifexp01(0, 1)
+    assert True == ifexp01(1, 1)
+    assert False == ifexp01(True, False)    
+
+test()
