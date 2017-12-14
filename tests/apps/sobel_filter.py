c1570ae2abf7f8ee80d9fc29cc21a4ba0686d10f
Hiroaki Kataoka <kataoka@sinby.com>
2017-12-14 12:03:41 +0900

    Update tests

--- tests/apps/sobel_filter.py
+++ tests/apps/sobel_filter.py
@@ -13,34 +13,43 @@ def min(a:int12, b:int12) -> int12:
     return a if a < b else b
 
 
-def normalize(v:int12):
-    return min(max(128 + v, 0), 255)
+def abs(a:int12) -> int12:
+    return a if a > 0 else -a
 
 
-K:Tuple[int4] = (-1, 0, 1,
-                 -2, 0, 2,
-                 -1, 0, 1)
+def clip(v:int12):
+    return min(max(v, 0), 255)
 
 
-def filter3x3(r0, r1, r2, r3, r4, r5, r6, r7, r8):
-    a0 = r0 * K[0]
-    a1 = r1 * K[1]
-    a2 = r2 * K[2]
-    a3 = r3 * K[3]
-    a4 = r4 * K[4]
-    a5 = r5 * K[5]
-    a6 = r6 * K[6]
-    a7 = r7 * K[7]
-    a8 = r8 * K[8]
-    s = a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8
-    return normalize(s)
+K_H:Tuple[int4] = (-1, 0, 1,
+                   -2, 0, 2,
+                   -1, 0, 1)
+
+K_V:Tuple[int4] = (-1, -2, -1,
+                   0,  0,  0,
+                   1,  2,  1)
+
+
+def filter3x3(r0, r1, r2, r3, r4, r5, r6, r7, r8, k):
+    a0 = r0 * k[0]
+    a1 = r1 * k[1]
+    a2 = r2 * k[2]
+    a3 = r3 * k[3]
+    a4 = r4 * k[4]
+    a5 = r5 * k[5]
+    a6 = r6 * k[6]
+    a7 = r7 * k[7]
+    a8 = r8 * k[8]
+    return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8
 
 
 @module
 class PipelinedStreamFilter:
     def __init__(self, width, height, blank_size):
-        self.inq = Queue(int12, 'in', maxsize=8)
-        self.outq = Queue(int12, 'out', maxsize=8)
+        self.inq = Queue(int12, 'in', maxsize=4)
+        #self.outhq = Queue(int12, 'out', maxsize=4)
+        #self.outvq = Queue(int12, 'out', maxsize=4)
+        self.outq = Queue(int12, 'out', maxsize=4)
         self.append_worker(self.worker, width, height, blank_size)
 
     def worker(self, width, height, blank_size):
@@ -63,6 +72,7 @@ class PipelinedStreamFilter:
         r8:int12 = 255
         while is_worker_running():
             for x in pipelined(range(blank_size, width + blank_size)):
+            #for x in range(blank_size, width + blank_size):
                 d2 = self.inq.rd()
                 line2[x] = d2
                 d1 = line1[x]
@@ -70,11 +80,18 @@ class PipelinedStreamFilter:
                 r0, r1, r2 = r1, r2, d0
                 r3, r4, r5 = r4, r5, d1
                 r6, r7, r8 = r7, r8, d2
-                out = filter3x3(r0, r1, r2,
-                                r3, r4, r5,
-                                r6, r7, r8)
-                print('out', out)
-                self.outq.wr(out)
+                out_h = filter3x3(r0, r1, r2,
+                                  r3, r4, r5,
+                                  r6, r7, r8, K_H)
+                out_v = filter3x3(r0, r1, r2,
+                                  r3, r4, r5,
+                                  r6, r7, r8, K_V)
+                #out_h_c = clip(128 + out_h)
+                #out_v_c = clip(128 + out_v)
+                out_c = clip(abs(out_h) + abs(out_v) >> 1)
+                #self.outhq.wr(out_h_c)
+                #self.outvq.wr(out_v_c)
+                self.outq.wr(out_c)
             phase = (phase + 1) % 3
             if phase == 0:
                 line0 = buf0
