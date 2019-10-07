256c4602e00fd59637a3afa3742487fec14b04a0
Hiroaki Kataoka <ktok07b6@gmail.com>
2019-10-07 17:54:24 +0900

    Update test

--- tests/apps/minivm2.py
+++ tests/apps/minivm2.py
@@ -1,5 +1,6 @@
 import polyphony
-from polyphony.io import Port, Queue, Handshake
+from polyphony import Channel
+from polyphony.io import Port, Handshake
 from polyphony.typing import int8, uint8, uint16
 
 
@@ -13,61 +14,52 @@ class MiniVM:
     HALT = 0xFF
 
     def __init__(self, start_addr, end_addr):
-        # define i/o
         self.dout = Handshake(int8, 'out')
+        self.ch_inst = Channel(uint16, capacity=2)
+        self.ch_ope  = Channel(uint16, capacity=2)
+        self.ch_value = Channel(uint16, capacity=2)
+        self.reg0 = 0
+        self.reg1 = 0
+        self.append_worker(self.fetch, start_addr, end_addr)
+        self.append_worker(self.decode)
+        self.append_worker(self.execute)
+        self.program = [0x0001, 0x0102, 0x0200, 0x0103, 0x0200, 0xFF00,  # 1 + 2 + 3
+                        0x0002, 0x0103, 0x0400, 0x0104, 0x0400, 0xFF00]  # 2 * 3 * 4
 
-        # define internals
-        instq = Queue(uint16, 'any', maxsize=2)
-        opeq = Queue(uint8, 'any',  maxsize=2)
-        valueq = Queue(uint8, 'any',  maxsize=2)
+    def fetch(self, start_addr, end_addr):
+        self.pc = start_addr
+        while polyphony.is_worker_running():
+            if end_addr < self.pc:
+                break
+            instruction = self.program[self.pc]
+            self.ch_inst.put(instruction)
+            self.pc += 1
 
-        self.append_worker(fetch, start_addr, end_addr, instq)
-        self.append_worker(decode, instq, opeq, valueq)
-        self.append_worker(execute, opeq, valueq, self.dout)
+    def decode(self):
+        while polyphony.is_worker_running():
+            inst = self.ch_inst.get()
+            op = (inst & 0xFF00) >> 8
+            v  = (inst & 0x00FF)
+            self.ch_ope.put(op)
+            self.ch_value.put(v)
 
-
-def fetch(start_addr, end_addr, instq):
-    pc = start_addr
-    program = [0x0001, 0x0102, 0x0200, 0x0103, 0x0200, 0xFF00,  # 1 + 2 + 3
-               0x0002, 0x0103, 0x0400, 0x0104, 0x0400, 0xFF00]  # 2 * 3 * 4
-
-    while polyphony.is_worker_running():
-        if end_addr < pc:
-            break
-        instruction = program[pc]
-        pc += 1
-        instq.wr(instruction)
-
-
-def decode(instq, opeq, valueq):
-    while polyphony.is_worker_running():
-        inst = instq.rd()
-        op = (inst & 0xFF00) >> 8
-        v  = (inst & 0x00FF)
-        opeq.wr(op)
-        valueq.wr(v)
-
-
-def execute(opeq, valueq, dout):
-    reg0 = 0
-    reg1 = 0
-    while polyphony.is_worker_running():
-        op = opeq.rd()
-        v = valueq.rd()
-
-        if op == MiniVM.LOAD0:
-            reg0 = v
-        elif op == MiniVM.LOAD1:
-            reg1 = v
-        elif op == MiniVM.ADD:
-            reg0 = reg0 + reg1
-        elif op == MiniVM.SUB:
-            reg0 = reg0 - reg1
-        elif op == MiniVM.MUL:
-            reg0 = reg0 * reg1
-        elif op == MiniVM.HALT:
-            dout.wr(reg0)
-            break
+    def execute(self):
+        while polyphony.is_worker_running():
+            op = self.ch_ope.get()
+            v = self.ch_value.get()
+            if op == MiniVM.LOAD0:
+                self.reg0 = v
+            elif op == MiniVM.LOAD1:
+                self.reg1 = v
+            elif op == MiniVM.ADD:
+                self.reg0 = self.reg0 + self.reg1
+            elif op == MiniVM.SUB:
+                self.reg0 = self.reg0 - self.reg1
+            elif op == MiniVM.MUL:
+                self.reg0 = self.reg0 * self.reg1
+            elif op == MiniVM.HALT:
+                self.dout.wr(self.reg0)
+                #break
 
 
 @polyphony.testbench
@@ -85,6 +77,5 @@ def vm1_test(vm1):
 # instantiate synthesizing module
 vm0 = MiniVM(0, 5)
 vm1 = MiniVM(6, 11)
-
 vm0_test(vm0)
 vm1_test(vm1)
