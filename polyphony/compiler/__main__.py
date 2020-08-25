943904981978d0e271e8b74d380e0365c5ff16f8
Hiroaki Kataoka <ktok07b6@gmail.com>
2020-08-25 19:52:20 +0900

    Revert debug_mode option

--- polyphony/compiler/__main__.py
+++ polyphony/compiler/__main__.py
@@ -819,7 +819,7 @@ def compile_plan():
 
 def setup(src_file, options):
     env.__init__()
-    env.dev_debug_mode = True#options.debug_mode
+    env.dev_debug_mode = options.debug_mode
     env.verbose_level = options.verbose_level if options.verbose_level else 0
     env.quiet_level = options.quiet_level if options.quiet_level else 0
     env.enable_verilog_dump = options.verilog_dump
