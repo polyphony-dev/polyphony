05c9cc57287edb31a664477e2c49b9601489837c
Hiroaki Kataoka <ktok07b6@gmail.com>
2026-03-25 11:58:10 +0900

    feat(csim): signal ID assignment for flat buffer layout

---
+++ polyphony/compiler/target/csim/csimgen.py
@@ -0,0 +1,73 @@
+from polyphony.compiler.ahdl.ahdl import AHDL, Ctx
+from polyphony.compiler.ahdl.ahdlvisitor import AHDLVisitor
+
+
+class AHDLToCTranspiler(AHDLVisitor):
+    """Transpiles AHDL to C code with zero-copy shared buffer."""
+
+    def __init__(self):
+        super().__init__()
+        self._sig_map: dict[str, int] = {}
+        self._port_map: dict[str, int] = {}
+        self._sig_count: int = 0
+        self._const_map: dict[str, int] = {}
+        self._sig_widths: list[tuple[int, bool]] = []
+        self._lines: list[str] = []
+
+    def assign_signal_ids(self, hdlscope):
+        self._sig_map = {}
+        self._port_map = {}
+        self._const_map = {}
+        self._sig_widths = []
+        idx = 0
+
+        signals = hdlscope.get_signals(
+            include_tags={'reg', 'net', 'regarray', 'netarray'},
+            exclude_tags={'input', 'output'},
+        )
+        for sig in signals:
+            if sig.is_constant() or sig.is_rom():
+                if sig.is_constant() and sig in hdlscope.constants:
+                    self._const_map[sig.name] = hdlscope.constants[sig]
+                continue
+            idx = self._assign_one(sig, idx)
+
+        port_signals = hdlscope.get_signals(include_tags={'input', 'output'})
+        for sig in port_signals:
+            if sig.is_constant() or sig.is_rom():
+                continue
+            if sig.name not in self._sig_map:
+                idx = self._assign_one(sig, idx)
+            self._port_map[sig.name] = self._sig_map[sig.name]
+
+        self._sig_count = idx
+        return dict(self._sig_map), dict(self._port_map), self._sig_count
+
+    def _assign_one(self, sig, idx):
+        is_signed = sig.is_int()
+        if sig.is_regarray():
+            elem_w, length = sig.width
+            self._sig_map[sig.name] = idx
+            for _ in range(length):
+                self._sig_widths.append((elem_w, is_signed))
+            self._sig_map[sig.name + '_next'] = idx + length
+            for _ in range(length):
+                self._sig_widths.append((elem_w, is_signed))
+            idx += 2 * length
+        elif sig.is_netarray():
+            _, length = sig.width
+            self._sig_map[sig.name] = idx
+            for _ in range(length):
+                self._sig_widths.append((sig.width[0], is_signed))
+            idx += length
+        elif sig.is_reg():
+            self._sig_map[sig.name] = idx
+            self._sig_widths.append((sig.width, is_signed))
+            self._sig_map[sig.name + '_next'] = idx + 1
+            self._sig_widths.append((sig.width, is_signed))
+            idx += 2
+        else:
+            self._sig_map[sig.name] = idx
+            self._sig_widths.append((sig.width, is_signed))
+            idx += 1
+        return idx
--- pytests/compiler/target/csim/test_csimgen.py
+++ pytests/compiler/target/csim/test_csimgen.py
@@ -15,3 +15,104 @@ def test_runtime_template_has_mask():
         src = f.read()
     assert 'mask' in src
     assert 'int64_t' in src
+
+
+from unittest.mock import MagicMock
+from polyphony.compiler.target.csim.csimgen import AHDLToCTranspiler
+
+
+def _make_signal(name, width, tags):
+    """Create a mock Signal with Tagged-compatible is_xxx() methods."""
+    sig = MagicMock()
+    sig.name = name
+    sig.width = width
+    sig.tags = set(tags)
+    sig.is_reg = lambda: 'reg' in sig.tags
+    sig.is_net = lambda: 'net' in sig.tags
+    sig.is_regarray = lambda: 'regarray' in sig.tags
+    sig.is_netarray = lambda: 'netarray' in sig.tags
+    sig.is_constant = lambda: 'constant' in sig.tags
+    sig.is_rom = lambda: 'rom' in sig.tags
+    sig.is_input = lambda: 'input' in sig.tags
+    sig.is_output = lambda: 'output' in sig.tags
+    sig.is_int = lambda: 'int' in sig.tags
+    sig.is_initializable = lambda: False
+    sig.init_value = 0
+    sig.is_subscope = lambda: 'subscope' in sig.tags
+    return sig
+
+
+def _make_hdlscope(signals, constants=None, subscopes=None):
+    scope = MagicMock()
+
+    def get_signals(include_tags=None, exclude_tags=None):
+        result = []
+        for sig in signals:
+            if exclude_tags and exclude_tags & sig.tags:
+                continue
+            if include_tags and not (include_tags & sig.tags):
+                continue
+            result.append(sig)
+        return result
+
+    scope.get_signals = get_signals
+    scope.constants = constants or {}
+    scope.subscopes = subscopes or {}
+    scope.name = 'test_module'
+    return scope
+
+
+def test_assign_signal_ids_scalar_reg_and_net():
+    reg_sig = _make_signal('state', 8, {'reg'})
+    net_sig = _make_signal('sum', 16, {'net'})
+    hdlscope = _make_hdlscope([reg_sig, net_sig])
+    tp = AHDLToCTranspiler()
+    sig_map, port_map, sig_count = tp.assign_signal_ids(hdlscope)
+    assert sig_map['state'] == 0
+    assert sig_map['state_next'] == 1
+    assert sig_map['sum'] == 2
+    assert sig_count == 3
+    assert port_map == {}
+
+
+def test_assign_signal_ids_with_ports():
+    in_sig = _make_signal('a', 32, {'net', 'input'})
+    out_sig = _make_signal('result', 32, {'reg', 'output'})
+    hdlscope = _make_hdlscope([in_sig, out_sig])
+    tp = AHDLToCTranspiler()
+    sig_map, port_map, sig_count = tp.assign_signal_ids(hdlscope)
+    assert port_map['a'] == sig_map['a']
+    assert port_map['result'] == sig_map['result']
+
+
+def test_assign_signal_ids_regarray():
+    arr_sig = _make_signal('mem', (32, 4), {'regarray'})
+    hdlscope = _make_hdlscope([arr_sig])
+    tp = AHDLToCTranspiler()
+    sig_map, port_map, sig_count = tp.assign_signal_ids(hdlscope)
+    assert sig_map['mem'] == 0
+    assert sig_map['mem_next'] == 4
+    assert sig_count == 8
+
+
+def test_assign_signal_ids_netarray():
+    arr_sig = _make_signal('wires', (16, 3), {'netarray'})
+    hdlscope = _make_hdlscope([arr_sig])
+    tp = AHDLToCTranspiler()
+    sig_map, port_map, sig_count = tp.assign_signal_ids(hdlscope)
+    assert sig_map['wires'] == 0
+    assert sig_count == 3
+
+
+def test_assign_signal_ids_skip_constant_and_rom():
+    const_sig = _make_signal('PARAM', 32, {'constant'})
+    rom_sig = _make_signal('rom0', 32, {'rom'})
+    reg_sig = _make_signal('x', 8, {'reg'})
+    hdlscope = _make_hdlscope([const_sig, rom_sig, reg_sig])
+    hdlscope.constants = {const_sig: 42}
+    tp = AHDLToCTranspiler()
+    sig_map, port_map, sig_count = tp.assign_signal_ids(hdlscope)
+    assert 'PARAM' not in sig_map
+    assert 'rom0' not in sig_map
+    assert sig_map['x'] == 0
+    assert sig_count == 2
